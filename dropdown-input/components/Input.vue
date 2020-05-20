8014891da6c99b00c43f7cb6eb604ae142d0b67b
Anton Sapega <antonsapega@gmail.com>
2020-05-20 18:36:50 +0300

    Basic styles and functional

--- dropdown-input/components/Input.vue
+++ dropdown-input/components/Input.vue
@@ -1,23 +1,62 @@
 <template>
   <div class="input-class">
-    <input type="text" placeholder="size" />
-		<ul>
-			<li>kg</li>
-			<li>cm</li>
-			<li>lit</li>
-			<li>gr</li>
-		</ul>
+    <form>
+      <label for="unit">Еденица</label>
+      <input type="text" id="unit" v-bind:value="selectedValue" v-on:focus="inputInFocus" v-on:blur="inputLoseFocus" v-bind:placeholder="changePlaceholder"/>
+
+      <ul v-bind:class="showList">
+        <li v-on:mouseover="hoverOnUnit" v-on:click="clickedElem($event)">{{unit[0]}}</li>
+        <li v-on:mouseover="hoverOnUnit" v-on:click="clickedElem">{{unit[1]}}</li>
+        <li v-on:mouseover="hoverOnUnit" v-on:click="clickedElem">{{unit[2]}}</li>
+        <li v-on:mouseover="hoverOnUnit" v-on:click="clickedElem">{{unit[3]}}</li>
+        <li v-on:mouseover="hoverOnUnit" v-on:click="clickedElem">{{unit[4]}}</li>
+        <li v-on:mouseover="hoverOnUnit" v-on:click="clickedElem">{{unit[5]}}</li>
+      </ul>
+    </form>
+    <!-- <button type="button" v-on:click="clickedElem">Click</button> Just for example that event "onclick" realy work -->
   </div>
 </template>
 
+
+
 <script>
 export default {
+  data(param) {
+    return {
+      unit: ["kg", "lit", "gr", "mm", "cm", "dc"],
+      selectedValue: '',
+      inFocus: false,
+      showList: "hidden",
+      changePlaceholder: "Enter  unit"
+    };
+  },
+  methods: {
+    inputInFocus() {
+      this.inFocus = true;
+      this.showList = "visible";
+      this.changePlaceholder = '';
+    },
+    inputLoseFocus() {
+      this.showList = "hidden";
+      this.changePlaceholder = 'Enter  unit';
+    },
+    hoverOnUnit(event) {
+      this.changePlaceholder = event.toElement.innerHTML;
+      // this.selectedValue = event.toElement.innerHTML;
 
+    },
+    clickedElem(event) {
+      
+      console.log(event.bubbles);
+    }
+  }
 };
 </script>
 
-<style>
 
+
+<style>
+/* СБРОС СТИЛЕЙ */
 *,
 *::before,
 *::after {
@@ -86,43 +125,78 @@ select {
   font: inherit;
 }
 
+body {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  letter-spacing: 2px;
+  color: rgb(90, 90, 90);
+}
 
 .input-class {
   display: flex;
-	flex-direction: column;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-	height: 100vh;
-	background-color: whitesmoke;
+  height: 100vh;
+  background-color: rgb(240, 238, 238);
+}
+
+form {
+  position: relative;
+}
+
+label {
+  margin: 0 0 5px 5px;
+  font-size: 18px;
 }
 
 input {
-	padding: 0 0 0 10px;
+  border: 1px solid rgb(240, 238, 238);
+  padding: 0 0 0 10px;
   display: block;
-	width: 250px;
-	height: 40px;
-	font-size: 16px;
-	border-width: 0px;
+  width: 250px;
+  height: 40px;
+  font-size: 16px;
+  border-width: 1px;
 }
 
-input:focus, input:active {
-	outline: none;
-	box-shadow: none;
-	border-width: 1px;
-	border-color: #80bdff;
+input:focus,
+input:active {
+  /* outline: none;
+	box-shadow: none; */
+  /* border-width: 1px;*/
+  border-color: #80bdff;
+  /* box-sizing: border-box; */
+  outline: 0;
+  outline-offset: -1px;
+  /* width: 250px;
+	height: 40px; */
 }
 
 ul {
-	list-style-type: none;
-	width: 250px;
-	background-color: white;
-	margin: 0px;
-	padding: 0 0 0 10px;
-	display: block;
+  list-style-type: none;
+  width: 250px;
+  background-color: white;
+  margin: 0px;
+  padding: 0 0 0 0;
+  display: block;
+  position: absolute;
+  top: 67px;
+}
+
+.hidden {
+  display: none;
+}
+
+.visible {
+  display: block;
+}
+
+li {
+  padding-left: 10px;
 }
 
 ul li:hover {
-	background-color: #80bdff;
-	cursor: pointer;
+  background-color: #80bdff;
+  cursor: pointer;
 }
 </style>
--- dropdown-input/pages/index.vue
+++ dropdown-input/pages/index.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <h1>Hello world</h1>
+    <h1>Dropdown input</h1>
 </template>
 
 <script>
@@ -10,5 +10,8 @@ export default {
 </script>
 
 <style>
-
+    h1 {
+        margin: 5px auto 5px 15px;
+        color: rgb(90, 90, 90);
+    }
 </style>
